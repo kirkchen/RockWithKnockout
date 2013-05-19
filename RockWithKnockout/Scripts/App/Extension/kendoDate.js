e8566bb844db4d7825cc152e573adb4d852d2c94
kirkchen <rwk0119@yahoo.com.tw>
2013-05-19 12:00:06 +0800

    update kendoDate

--- RockWithKnockout/Scripts/App/Extension/kendoDate.js
+++ RockWithKnockout/Scripts/App/Extension/kendoDate.js
@@ -1,7 +1,15 @@
 ﻿ko.bindingHandlers.date = {
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {        
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        // First get the latest data that we're bound to
+        var value = valueAccessor();
+
         $(element).kendoDatePicker({
-            format: 'yyyy/MM/dd'
+            format: 'yyyy/MM/dd',
+            change: function (e) {
+                if (ko.isObservable(value)) {
+                    value(kendo.toString(this.value(), "yyyy/MM/dd"));
+                }
+            }
         });
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
